9192fb7317e963dc0816d634786ce16214bf6256
shahanfathimak <shahanfathima300@gmail.com>
2024-11-12 10:47:45 +0530

    Add files via upload

---
+++ polilines.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+img = cv.imread('kunjippa.jpeg')
+pts=np.array([[[100,400]],[[200,100]],[[300,400]],[[400,100]]], dtype=np.int32)
+cv.polylines(img,[pts],True,(255,0,0),5)
+cv.imshow('image',img)
+img2=plt.imread('kunjippa.jpeg')
+plt.imshow(img2)
+plt.show()
+
+
+cv.waitKey(0)
+cv.destroyAllWindows()
